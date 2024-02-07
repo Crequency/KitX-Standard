65986d0527a97d723d7ac7548c6a1da9f6a58dae
Dynesshely <catrol@qq.com>
2024-02-08 04:58:25 +0800

    ﻿💾 Feat: Optimized.

--- KitX.Shared/Device/DeviceLocator.cs
+++ KitX.Shared/Device/DeviceLocator.cs
@@ -9,4 +9,28 @@ public struct DeviceLocator
     public string IPv6 { get; set; }
 
     public string MacAddress { get; set; }
+
+    public DeviceLocator ResetDeviceName(string name)
+    {
+        DeviceName = name;
+        return this;
+    }
+
+    public DeviceLocator ResetIPv4(string ipv4)
+    {
+        IPv4 = ipv4;
+        return this;
+    }
+
+    public DeviceLocator ResetIPv6(string ipv6)
+    {
+        IPv6 = ipv6;
+        return this;
+    }
+
+    public DeviceLocator ResetMacAddress(string mac)
+    {
+        MacAddress = mac;
+        return this;
+    }
 }
