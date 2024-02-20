ee8d67d9d896fee0b4135667b31acdab6fd534b5
Dynesshely <catrol@qq.com>
2024-02-21 05:22:51 +0800

    💾 Feat(SDK, Standard): Adapt `Loader.CSharp` and `TestPlugin.WPF.Core` to latest Contract.

--- KitX.Shared/WebCommand/Command.cs
+++ KitX.Shared/WebCommand/Command.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KitX.Shared.Plugin;
+using System;
 using System.Collections.Generic;
 
 namespace KitX.Shared.WebCommand;
@@ -7,15 +8,11 @@ public struct Command
 {
     public CommandType Type { get; set; }
 
-    public int CallId { get; set; }
-
-    public int CallIdTTL { get; set; }
-
     public DateTime SendTime { get; set; }
 
     public string Request { get; set; }
 
-    public List<string> RequestArgs { get; set; }
+    public List<Parameter> RequestArgs { get; set; }
 
     public byte[] Body { get; set; }
 
---
+++ KitX.Shared/WebCommand/Details/PluginWorkingDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace KitX.Shared.WebCommand.Details;
+
+public class PluginWorkingDetail
+{
+    public string? PluginSaveDirectory { get; set; }
+
+    public string? PluginDataDirectory { get; set; }
+}
