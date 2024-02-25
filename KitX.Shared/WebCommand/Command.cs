d190b009e11673245054b9285237106b8f63183d
Dynesshely <catrol@qq.com>
2024-02-25 19:10:40 +0800

    💾 Feat: The first time that dashboard invoke plugin function throw network connection.

--- KitX.Shared/WebCommand/Command.cs
+++ KitX.Shared/WebCommand/Command.cs
@@ -6,17 +6,17 @@ namespace KitX.Shared.WebCommand;
 
 public struct Command
 {
-    public CommandType Type { get; set; }
-
     public DateTime SendTime { get; set; }
 
     public string Request { get; set; }
 
-    public List<Parameter> RequestArgs { get; set; }
-
     public byte[] Body { get; set; }
 
     public int BodyLength { get; set; }
+
+    public string FunctionName { get; set; }
+
+    public List<Parameter> FunctionArgs { get; set; }
 
     public Dictionary<string, string> Tags { get; set; }
 }
