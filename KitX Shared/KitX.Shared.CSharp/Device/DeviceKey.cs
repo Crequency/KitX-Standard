015743cc9c4839b732befa86b5603603a0289124
Dynesshely <catrol@qq.com>
2024-03-02 11:19:25 +0800

    💾 Feat(Dashboard): Enable to input verification code.

---
+++ KitX Shared/KitX.Shared.CSharp/Device/DeviceKey.cs
@@ -0,0 +1,14 @@
+﻿namespace KitX.Shared.CSharp.Device;
+
+public class DeviceKey
+{
+    public DeviceLocator Device { get; set; } = new();
+
+    public string? RsaPublicKeyModulus { get; set; }
+
+    public string? RsaPublicKeyExponent { get; set; }
+
+    public string? RsaPrivateKeyModulus { get; set; }
+
+    public string? RsaPrivateKeyD { get; set; }
+}
