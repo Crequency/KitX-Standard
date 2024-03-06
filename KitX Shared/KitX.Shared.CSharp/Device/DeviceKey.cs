c7f283521f9f560689513ab3bf651fabf7e53aca
Dynesshely <catrol@qq.com>
2024-03-06 08:50:35 +0800

    💾 Feat(Dashboard): Changed to save RSA key as PEM file and added device state indicator.

--- KitX Shared/KitX.Shared.CSharp/Device/DeviceKey.cs
+++ KitX Shared/KitX.Shared.CSharp/Device/DeviceKey.cs
@@ -4,13 +4,21 @@ public class DeviceKey
 {
     public DeviceLocator Device { get; set; } = new();
 
-    public string? RsaPublicKeyModulus { get; set; }
+    //[Obsolete]
+    //public string? RsaPublicKeyModulus { get; set; }
 
-    public string? RsaPublicKeyExponent { get; set; }
+    //[Obsolete]
+    //public string? RsaPublicKeyExponent { get; set; }
 
-    public string? RsaPrivateKeyModulus { get; set; }
+    //[Obsolete]
+    //public string? RsaPrivateKeyModulus { get; set; }
 
-    public string? RsaPrivateKeyD { get; set; }
+    //[Obsolete]
+    //public string? RsaPrivateKeyD { get; set; }
+
+    public string? RsaPublicKeyPem { get; set; }
+
+    public string? RsaPrivateKeyPem { get; set; }
 }
 
 public static class DeviceKeyExtensions
@@ -19,11 +27,17 @@ public static class DeviceKeyExtensions
     {
         var result = true;
 
-        if (key.RsaPublicKeyExponent is not null && key.RsaPublicKeyModulus is not null)
-            result = result && key.RsaPublicKeyExponent == other.RsaPublicKeyExponent && key.RsaPublicKeyModulus == other.RsaPublicKeyModulus;
+        if (key.RsaPublicKeyPem is not null && other.RsaPublicKeyPem is not null)
+            result = result && key.RsaPublicKeyPem == other.RsaPublicKeyPem;
 
-        if (key.RsaPrivateKeyD is not null && key.RsaPrivateKeyModulus is not null)
-            result = result && key.RsaPrivateKeyD == other.RsaPrivateKeyD && key.RsaPrivateKeyModulus == other.RsaPrivateKeyModulus;
+        if (key.RsaPrivateKeyPem is not null && other.RsaPrivateKeyPem is not null)
+            result = result && key.RsaPrivateKeyPem == other.RsaPrivateKeyPem;
+
+        //if (key.RsaPublicKeyExponent is not null && key.RsaPublicKeyModulus is not null)
+        //    result = result && key.RsaPublicKeyExponent == other.RsaPublicKeyExponent && key.RsaPublicKeyModulus == other.RsaPublicKeyModulus;
+
+        //if (key.RsaPrivateKeyD is not null && key.RsaPrivateKeyModulus is not null)
+        //    result = result && key.RsaPrivateKeyD == other.RsaPrivateKeyD && key.RsaPrivateKeyModulus == other.RsaPrivateKeyModulus;
 
         return result && key.Device.IsSameDevice(other.Device);
     }
